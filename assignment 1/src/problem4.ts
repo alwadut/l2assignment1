899f21b667e0ad8b58fd6fd22cd1798f8d93d924
alwadut <wadutchowdhary2042@gmail.com>
2025-05-08 13:08:29 +0600

    pblm4

---
+++ assignment 1/src/problem4.ts
@@ -0,0 +1,41 @@
+{
+    //
+    class Vehical {
+        Make:string
+        Year:number
+       
+
+
+
+        constructor (make:string,year:number){
+            this.Make =make;
+            this.Year=year;
+           
+        }
+        getInfo():string{
+            return`Make:${this.Make},Year ${this.Year}`;
+        }
+
+    }
+
+    class Car extends Vehical{
+        Model:string;
+        
+
+        constructor(  make:string, year:number,model:string){
+            super(make,year);
+            this.Model = model;
+        }
+        getModel():string{
+            return `Model :${this.Model}`
+        }
+    }
+
+    const myCar = new Car ("toyota",2000,"Corolla")
+    console.log(myCar.getInfo());
+    console.log(myCar.getModel());
+
+  
+
+    //
+}
